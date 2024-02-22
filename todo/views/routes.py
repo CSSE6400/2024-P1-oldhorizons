8eeccfc2eb0112601f630852979b904337561fd9
Michelle Sands <michelle.a.sands@gmail.com>
2024-02-22 18:53:06 +1000

    finish prac 01

---
+++ todo/views/routes.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify
+
+api = Blueprint('api', __name__, url_prefix='/api/v1')
+
+@api.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+
+@api.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify([])
+
+@api.route('/todos/<int:id>', methods=['GET'])
+def get_todo(id):
+    return jsonify(id)
+
+@api.route('/todos', methods=['POST'])
+def create_todo():
+    return jsonify({})
+
+@api.route('/todos/<int:id>', methods=['PUT'])
+def update_todo(id):
+    return jsonify(id)
+
+@api.route('/todos/<int:id>', methods=['DELETE'])
+def delete_todo(id):
+    return jsonify({})
